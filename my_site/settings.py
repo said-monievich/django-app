ab69c9ebe35b162f595ca33c4d3e62f96131387f
Said Monievich <said.monievich@gmail.com>
2025-04-10 20:38:17 +0500

    changes

--- my_site/settings.py
+++ my_site/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -32,7 +33,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "blog",
-    "book_outlet",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
